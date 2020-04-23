fdefa7100ceb6c6d53cfc860f1b91b200934e6c8
Роман Данилевич <dwordchanel@gmail.com>
2020-04-23 04:55:15 +0300

    Add project files.

---
+++ OOP_lab_3_7_3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace OOP_lab_3_7_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader fileInput = new StreamReader("input.txt");
+
+            string sentence = fileInput.ReadLine();
+
+            fileInput.Close();
+
+            string[] elements = sentence.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            int k = 0;
+
+            foreach(string element in elements)
+            {
+                int k1 = 0;
+
+                foreach(char c in element)
+                {
+                    if ((c >= (char)48) && (c <= (char)57)) ++k1;
+                }
+
+                if (k1 == element.Length) ++k;
+            }
+
+            StreamWriter fileOutput = File.CreateText("output.txt");
+
+            fileOutput.WriteLine(k);
+
+            fileOutput.Close();
+        }
+    }
+}
